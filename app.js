39d39f557020a1cb35e8cde387e917595aba1ec9
Yaseen YK <45963355+Yaseenyk@users.noreply.github.com>
2023-07-31 05:57:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const search = document.getElementById("search-meal");
 const submit = document.getElementById("submit-meal");
 const mainContainer = document.querySelector(".main-container");
+const favouriteContainer = document.querySelector(".favourite-meals");
 const Popup = document.getElementById("Popup");
 let mealsData = []; // To store the meals data
+let favouriteMeals = JSON.parse(localStorage.getItem("favouriteMeals")) || [];
 
 submit.onclick = () => {
   const mealText = search.value.trim();
@@ -17,21 +19,17 @@ submit.onclick = () => {
           mealsData = data.meals; // Store the meals data in the variable
 
           mainContainer.innerHTML = ""; // Clear previous content
-          const mealsHTML = mealsData
-            .map(
-              (meal) =>
-                `<div class="inner-container-meal">
-                      <img src='${meal.strMealThumb}' class='mealImg'/>
-                      <div class="meal-item">${meal.strMeal}</div>
-                    </div>`
-            )
-            .join("");
+          const mealsHTML = mealsData.map((meal) => `
+            <div class="inner-container-meal">
+              <img src='${meal.strMealThumb}' class='mealImg'/>
+              <div class="meal-item">${meal.strMeal}</div>
+              <button class="add-favourite-button" onclick="addToFavourites('${meal.idMeal}')">Add to Favourites</button>
+            </div>
+          `).join("");
           mainContainer.innerHTML = mealsHTML;
 
           // Add event listeners to each inner-container-meal to show the popup
-          const mealItems = Array.from(
-            document.querySelectorAll(".inner-container-meal")
-          );
+          const mealItems = Array.from(document.querySelectorAll(".inner-container-meal"));
           mealItems.map((mealItem) => {
             mealItem.addEventListener("click", () => {
               const mealName = mealItem.querySelector(".meal-item").innerText;
@@ -45,8 +43,7 @@ submit.onclick = () => {
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-        
-          alert("Sorry, The Dish is Not on our Menu Plate!");
+        alert("Sorry, The Dish is Not on our Menu Plate!");
       });
   } else {
     alert("Sorry, can you enter the dish you want?");
@@ -60,27 +57,26 @@ function displayMealPopup(meal) {
   const instructions = meal.strInstructions;
 
   const popupHTML = `
-        <div class="popup-content">
-          <div class='aligning-item'>
-            <h2 class='meal-name-popup'>${mealName}</h2>
-            <span class="close-popup" onclick="closeMealPopup()">&times;</span>
-          </div>
-          <div class='display-side'>
-          <img src="${mealImage}" class="popup-meal-img"/>
-          <div class="meal-details">
-            <p class='P-tag'><strong class='Ing'>Ingredients:</strong></p>
-            <ol class='ol-Tag'>
-              ${ingredientsList}
-            </ol>
-            </div>
-            </div>
-            <div class='instructions'>
-            <p><strong>Instructions:</strong></p>
-            <p>${instructions}</p>
-            </div>
-          
+    <div class="popup-content">
+      <div class='aligning-item'>
+        <h2 class='meal-name-popup'>${mealName}</h2>
+        <span class="close-popup" onclick="closeMealPopup()">&times;</span>
+      </div>
+      <div class='display-side'>
+        <img src="${mealImage}" class="popup-meal-img"/>
+        <div class="meal-details">
+          <p class='P-tag'><strong class='Ing'>Ingredients:</strong></p>
+          <ol class='ol-Tag'>
+            ${ingredientsList}
+          </ol>
         </div>
-      `;
+      </div>
+      <div class='instructions'>
+        <p><strong>Instructions:</strong></p>
+        <p>${instructions}</p>
+      </div>
+    </div>
+  `;
 
   Popup.innerHTML = popupHTML;
   Popup.style.display = "block";
@@ -102,3 +98,38 @@ function closeMealPopup() {
   Popup.innerHTML = "";
   Popup.style.display = "none";
 }
+
+// Function to update the favourite meals list
+function updateFavouriteMeals() {
+  favouriteContainer.innerHTML = "";
+  favouriteMeals.forEach((meal) => {
+    const favouriteMealHTML = `
+      <div class="favourite-meal">
+        <img src="${meal.strMealThumb}" class="favourite-meal-img" />
+        <div class="favourite-meal-name">${meal.strMeal}</div>
+        <button class="remove-favourite-button" onclick="removeFromFavourites('${meal.idMeal}')">Remove</button>
+      </div>
+    `;
+    favouriteContainer.innerHTML += favouriteMealHTML;
+  });
+}
+
+// Function to add a meal to favourites
+function addToFavourites(mealId) {
+  const mealToAdd = mealsData.find((meal) => meal.idMeal === mealId);
+  if (mealToAdd) {
+    favouriteMeals.push(mealToAdd);
+    localStorage.setItem("favouriteMeals", JSON.stringify(favouriteMeals));
+    updateFavouriteMeals();
+  }
+}
+
+// Function to remove a meal from favourites
+function removeFromFavourites(mealId) {
+  favouriteMeals = favouriteMeals.filter((meal) => meal.idMeal !== mealId);
+  localStorage.setItem("favouriteMeals", JSON.stringify(favouriteMeals));
+  updateFavouriteMeals();
+}
+
+// Call updateFavouriteMeals() to display the initial favourite meals
+updateFavouriteMeals();
